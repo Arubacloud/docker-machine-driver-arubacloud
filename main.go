1ab8b8a157ebd342f30a3438ccd643fa878a2b59
ne29111984 <nicola.eusebi@gmail.com>
2017-03-02 12:04:11 +0100

    remove unused consts

--- main.go
+++ main.go
@@ -4,14 +4,6 @@ import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
 )
 
-const (
-	DefaultSecurityGroup = "default"
-	DefaultProjectName   = "docker-machine"
-	DefaultTemplateID    = "401"
-	ImageName            = "Ubuntu 14.04"
-	SshUserName          = "root"
-)
-
 func main() {
 	plugin.RegisterDriver(new(Driver))
 }
